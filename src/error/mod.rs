f531a4b0a2ace58c362685e53dd87b61c8eae9ef
Ancient <arthas.you@163.com>
2025-05-28 16:35:15 +0800

    fix: removing some warning

--- src/error/mod.rs
+++ src/error/mod.rs
@@ -4,9 +4,6 @@ use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum Error {
-    #[error("system error: {0}")]
-    SystemError(String),
-
     #[error("io error: {0}")]
     IoError(#[from] std::io::Error),
 
@@ -18,16 +15,6 @@ pub enum Error {
 
     #[error("db error: {0}")]
     DbError(#[from] surrealdb::Error),
-
-    #[error("{message:} ({line:}, {column})")]
-    CustomError {
-        message: String,
-        line: u32,
-        column: u32,
-    },
-
-    #[error("error code: {0}")]
-    ErrorCode(u16),
 }
 
 pub type Result<T, E = Error> = core::result::Result<T, E>;
