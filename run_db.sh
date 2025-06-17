ac7a29d5f1e546354b789db09375d2b18d65c180
Ancient <arthas.you@163.com>
2025-06-17 12:25:20 +0800

    fix: no permission bug

--- run_db.sh
+++ run_db.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 docker rm -f surrealdb
+
 docker run -d \
     --name surrealdb \
     --restart=always \
-    -v `pwd`/authdata:/authdata \
+    -v `pwd`/testdata:/testdata \
+    --user $(id -u) \
     -p 8000:8000 \
-    surrealdb/surrealdb:latest start --user root --pass root rocksdb:/authdata/authbase.db
+    surrealdb/surrealdb:latest start --user root --pass root rocksdb:/testdata/testbase.db
